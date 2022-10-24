961a6898f596fccc538e4bff38cf4a597453949e
Kalsito1017 <kurabiika34@abv.bg>
2022-10-24 19:31:50 +0300

    Added Inheritance Exercises

---
+++ Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        
+        public string RandomString()
+        {
+            Random random = new Random();
+            int index = random.Next(this.Count);
+            string removed = this[index];
+            this.RemoveAt(index);
+            return removed;
+        }
+    }
+}
