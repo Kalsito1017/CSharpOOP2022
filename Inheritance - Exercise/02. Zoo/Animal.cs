9f742c57fc3a909e81c414cf25875c695807c2a3
Kalsito1017 <kurabiika34@abv.bg>
2022-10-24 21:30:09 +0300

    Added Inheritance Exercises

---
+++ Inheritance - Exercise/02. Zoo/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    
+        public class Animal
+    {
+        protected string name;
+
+        public string Name { get => this.name; set => this.name = value; }
+        public Animal(
+            string name)
+        {
+            Name = name;
+        }
+    }
+}
