8e603168111d183012e2a159f9f123fe63a54b1a
Kalsito1017 <kurabiika34@abv.bg>
2022-11-22 20:18:41 +0200

    Added StartUp.cs

---
+++ !Exams/C# OOP Exam – 14 Aug 2022/PlanetWars/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace Gym
+{
+    using Gym.Core;
+    using Gym.Core.Contracts;
+    using Gym.Models.Athletes;
+    using Gym.Models.Equipment;
+    using Gym.Models.Gyms;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //Testing Structure Below !!!
+            //BoxingGym gym = new BoxingGym("BoxingGym");
+            //gym.AddAthlete(new Boxer("Joro dqsnoto", "dokrai", 0));
+            //gym.Exercise();
+            //gym.AddEquipment(new BoxingGloves());
+            //System.Console.WriteLine(gym.GymInfo());
+            // Don't forget to comment out the commented code lines in the Engine class!
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
